620690e13d5650b6ad18c3fab011a9bf1f715b9b
Ekaterina Belyakova <Ekaterina_Belyakova@epam.com>
2021-03-08 18:01:53 +0300

    refactor: fixes after review

--- src/components/MovieCard/movieCard.jsx
+++ src/components/MovieCard/movieCard.jsx
@@ -7,31 +7,33 @@ export const MovieCard = ({ movie }) => (
   <div className="movie-card">
     <img
       className="movie-card__image"
-      src={movie.Poster}
-      alt={movie.Title}
+      src={movie.poster}
+      alt={movie.title}
     />
     <div className="movie-card__info">
       <div className="movie-card__descr">
-        <h2 className="movie-card__title">{movie.Title}</h2>
-        <p className="movie-card__genre">Comedy</p>
+        <h2 className="movie-card__title">{movie.title}</h2>
+        <p className="movie-card__genre">{movie.genre}</p>
       </div>
-      <div className="movie-card__release-date">{movie.Year}</div>
+      <div className="movie-card__release-date">{movie.year}</div>
     </div>
   </div>
 );
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    Title: PropTypes.string.isRequired,
-    Poster: PropTypes.string.isRequired,
-    Year: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    year: PropTypes.string.isRequired,
   }),
 };
 
 MovieCard.defaultProps = {
   movie: {
-    Title: 'Missing Title',
-    Poster: 'Missing Poster',
-    Year: 'Missing Year',
+    title: 'Missing Title',
+    poster: 'Missing Poster',
+    genre: 'Missing Genre',
+    year: 'Missing Year',
   },
 };
--- src/components/Sorting/sorting.jsx
+++ src/components/Sorting/sorting.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 import './sorting.scss';
 
+import { sortingItems } from '../../mockedData';
+
 export const Sorting = () => (
   <div className="sorting">
     <span>sort by</span>
     <select>
-      <option value="value1" defaultValue>release date</option>
-      <option value="value2">rating</option>
+      {sortingItems.map((item) => <option value={item}>{item}</option>)}
     </select>
   </div>
 );
