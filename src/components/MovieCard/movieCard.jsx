1c69f256ff7c37bfdda78e42e12b631682b43b76
Ekaterina Belyakova <Ekaterina_Belyakova@epam.com>
2021-03-08 23:15:18 +0300

    feat: add menu on MovieCard

--- src/components/MovieCard/movieCard.jsx
+++ src/components/MovieCard/movieCard.jsx
@@ -1,24 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import './movieCard.scss';
 
-export const MovieCard = ({ movie }) => (
-  <div className="movie-card">
-    <img
-      className="movie-card__image"
-      src={movie.poster}
-      alt={movie.title}
-    />
-    <div className="movie-card__info">
-      <div className="movie-card__descr">
-        <h2 className="movie-card__title">{movie.title}</h2>
-        <p className="movie-card__genre">{movie.genre}</p>
+import menuIcon from '../../images/more.svg';
+import closeIcon from '../../images/cancel.svg';
+
+export const MovieCard = ({ movie }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuBtnShown, setIsMenuBtnShown] = useState(false);
+
+  return (
+    <div
+      className="movie-card"
+      onMouseEnter={() => setIsMenuBtnShown(true)}
+      onMouseLeave={() => setIsMenuBtnShown(false)}
+    >
+      {isMenuBtnShown && (
+        <button
+          className="movie-card__menu-btn"
+          type="button"
+          onClick={() => setIsMenuOpen(true)}
+        >
+          <img src={menuIcon} alt="menu" />
+        </button>
+      )}
+
+      {isMenuOpen && (
+        <div className="movie-card__menu">
+          <button
+            className="movie-card__menu-close-btn"
+            type="button"
+            onClick={() => setIsMenuOpen(false)}
+          >
+            <img src={closeIcon} alt="close" />
+          </button>
+          <button className="movie-card__menu-item" type="button">Edit</button>
+          <button className="movie-card__menu-item" type="button">Delete</button>
+        </div>
+      )}
+
+      <img
+        className="movie-card__image"
+        src={movie.poster}
+        alt={movie.title}
+      />
+      <div className="movie-card__info">
+        <div className="movie-card__descr">
+          <h2 className="movie-card__title">{movie.title}</h2>
+          <p className="movie-card__genre">{movie.genre}</p>
+        </div>
+        <div className="movie-card__release-date">{movie.year}</div>
       </div>
-      <div className="movie-card__release-date">{movie.year}</div>
     </div>
-  </div>
-);
+  );
+};
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
